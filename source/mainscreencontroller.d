6672401f3c53855f11d8e80654e8b1ad59d8c80d
Supatier <atisuperati@gmail.com>
2018-03-27 16:06:49 +0700

    update 2

--- source/mainscreencontroller.d
+++ source/mainscreencontroller.d
@@ -12,23 +12,27 @@ class MainScreenController : GodotScript!Node {
                 print("Main Screen Controller is ready!");
         }
 
-        @Method void _on_ExampleOneButton_pressed() {
-                this.getTree().changeScene("res://scene/life_cycle_scene.tscn");
+        @Method @Rename("_on_example_one_button_pressed")
+        void _onExampleOneButtonPressed() {
+                getTree().changeScene("res://scene/life_cycle_scene.tscn");
                 print("Life Cycle Scene loaded!");
         }
 
-        @Method void onExampleTwoButtonPressed() {
-                this.getTree().changeScene("res://scene/input_handling_scene.tscn");
+        @Method @Rename("_on_example_two_button_pressed")
+        void _onExampleTwoButtonPressed() {
+                getTree().changeScene("res://scene/input_handling_scene.tscn");
                 print("Input Handling Scene loaded!");
         }
 
-        @Method void onExampleThreeButtonPressed() {
-                this.getTree().changeScene("res://scene/basic_3d_physics_scene.tscn");
+        @Method @Rename("_on_example_three_button_pressed")
+        void _onExampleThreeButtonPressed() {
+                getTree().changeScene("res://scene/basic_3d_physics_scene.tscn");
                 print("Basic 3D Physics Scene loaded!");
         }
 
-        @Method void onExampleFourButtonPressed() {
-                this.getTree().changeScene("res://scene/moving_kinematicbody_scene.tscn");
+        @Method @Rename("_on_example_four_button_pressed")
+        void _onExampleFourButtonPressed() {
+                getTree().changeScene("res://scene/moving_kinematicbody_scene.tscn");
                 print("Moving KinematicBody Scene loaded!");
         }
 }
